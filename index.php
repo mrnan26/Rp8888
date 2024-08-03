bf7e850638f87917fa15c88d82a8455bd5ec2986
Mr.Nobody26 <112294636+mrnan26@users.noreply.github.com>
2024-08-03 15:49:15 +0700

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,186 @@
+<!DOCTYPE html>
+<html ⚡ lang="id-ID" itemscope="itemscope" itemtype="https://schema.org/WebPage">
+<!-- This site is optimized with XAL26 SEO -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="id" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#" lang="id" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+    <title>PARLAY4D - Agen BOLA  Online | Daftar & Login PARLAY4D  | Link Alternatif PARLAY4D | http://parlay4d.cc/?ref=yoyo1123</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="llu6KUw0TXiGQub13gIQnnMfTGkvBpPEqj4RGr0Ju8g" />
+<meta name="description" content="PARLAY4D adalah Situs BOLA Online Terbesar di Indonesia yang memberikan keuntungan lebih kepada membernya. Daftar PARLAY4D, http://parlay4d.cc/?ref=yoyo1123, Link Alternatif PARLAY4D, Login PARLAY4D dan dilayani CS yang ramah!!">
+<meta name="keywords" content="http://parlay4d.cc/?ref=yoyo1123, Daftar PARLAY4D, Link Alternatif http://parlay4d.cc/?ref=yoyo1123, Login PARLAY4D, Situs BOLA SLOT  Online Terbesar di Indonesia">
+<link rel="amphtml" href="http://parlay4d.cc/?ref=yoyo1123/m/">
+<link rel="canonical" href="http://parlay4d.cc/?ref=yoyo1123"/>
+<meta property="og:url" content="http://parlay4d.cc/?ref=yoyo1123"/>
+<link rel="shortcut icon" href="https://i.postimg.cc/kGJ6mxzv/image-2021-10-31-222126.png" type="https://i.postimg.cc/kGJ6mxzv/image-2021-10-31-222126.png" sizes="16x16">
+<style type="text/css">
+.myButton {
+	-moz-box-shadow: 0px 1px 0px 0px #050505;
+	-webkit-box-shadow: 0px 1px 0px 0px #050505;
+	box-shadow: 0px 1px 0px 0px #050505;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #e600e2), color-stop(1, #71009e));
+	background:-moz-linear-gradient(top, #e600e2 5%, #71009e 100%);
+	background:-webkit-linear-gradient(top, #e600e2 5%, #71009e 100%);
+	background:-o-linear-gradient(top, #e600e2 5%, #71009e 100%);	
+	background:-ms-linear-gradient(top, #e600e2 5%, #71009e 100%);
+	background:linear-gradient(to bottom, #07c6f1  5%, #0c2e3e 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e600e2', endColorstr='#71009e',GradientType=0);
+	background-color:#e600e2;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #080808;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:23px;
+	font-weight:bold;
+	padding:13px 30px;
+	text-decoration:none;
+	text-shadow:0px -1px 0px #000000;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #71009e), color-stop(1, #e600e2));
+	background:-moz-linear-gradient(top, #71009e 5%, #e600e2 100%);
+	background:-webkit-linear-gradient(top, #71009e 5%, #e600e2 100%);
+	background:-o-linear-gradient(top, #71009e 5%, #e600e2 100%);
+	background:-ms-linear-gradient(top, #71009e 5%, #e600e2 100%);
+	background:linear-gradient(to bottom, #0c2e3e 5%, #2196F3 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#71009e', endColorstr='#e600e2',GradientType=0);
+	background-color:#71009e;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
 
+
+
+#footer{height: 100%; min-width: 100%; top center repeat-x;}
+#menu-footer, #menu ul{margin: 0; padding: 0; list-style: none;}
+#menu-footer{margin: 0px 0 5px 0; }
+#menu-footer li{display: inline-block; padding: 0 5px; border-left: 1px solid #EFE390;}
+#menu-footer li:first-child{border: none;}
+#menu-footer li a{color: #fff;}
+body{
+background: #000  url('') fixed no-repeat top center;background-size:100% 100%;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;
+</style>
+ <script type="text/javascript">
+	$(document).ready(function() {
+		var delay = 46800;
+		var jackpot = 0;
+		pull_jackpot();
+		var timer;
+		function pull_jackpot() {
+		var nominal = 1383806986;
+		if (jackpot == 0) jackpot = parseInt(nominal - 100000000);
+		var amount = 1000000 / delay * 1;
+		timer = setInterval(function() { jackpot = set_jackpot(jackpot, nominal, amount); }, 1);
+		}
+
+		function set_jackpot(jackpot, nominal, amount) {
+			var jackpot = jackpot + amount;
+			if (jackpot >= nominal) {
+				clearInterval(timer);
+				pull_jackpot();
+			} else {
+				var result = addCommas(parseInt(jackpot));
+				$('#jp').html(result);
+			}
+			return jackpot;
+		}
+		function addCommas(nStr) {
+			nStr += '';
+			x = nStr.split('.');
+			x1 = x[0];
+			x2 = x.length > 1 ? '.' + x[1] : '';
+			var rgx = /(\d+)(\d{3})/;
+			while (rgx.test(x1)) {
+				x1 = x1.replace(rgx, '$1' + ',' + '$2');
+			}
+			return x1 + x2;
+		}
+	})
+</script>
+
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-158699480-1');
+</script>
+
+<script language="javascript">
+var win = null;
+function NewWindow(mypage,myname,w,h,scroll){
+LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+settings =
+'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+win = window.open(mypage,myname,settings)
+}
+</script>
+</head>
+<body>
+<center>
+<table style="border:0px solid #CC0000; box-shadow: 0 0 0px 0px #CC0000; border-radius: 10px; background: -moz-linear-gradient(top, #FFCC6640%, #71009e 100%);
+	background:-webkit-linear-gradient(top, rgba(0, 0, 0, 0.6) 30%, #71009e 100%);
+	background:-o-linear-gradient(top, rgba(0, 0, 0, 0.6) 30%, #71009e 100%);
+	background:-ms-linear-gradient(top, rgba(0, 0, 0, 0.6) 30%, #71009e 100%);
+	background:linear-gradient(to bottom, rgba(0, 0, 0, 0.6) 30%, #08a2c7 100%); padding:0px 15px;">    
+<tbody><tr>
+      <td>
+<center><span itemscope="itemscope" itemtype="http://schema.org/Brand"><a itemprop="url" href="/"><img itemprop="logo" src="https://i.postimg.cc/kGJ6mxzv/image-2021-10-31-222126.png" alt="datukqq" width="250" style="min-width:35%;"></a><meta itemprop="name" content="BOLA online"></span><br><br>
+<a href="http://parlay4d.cc/?ref=yoyo1123" rel="nofollow" class="myButton">L O G I N</a>&nbsp;&nbsp;
+ <a href="http://parlay4d.cc/?ref=yoyo1123" rel="nofollow" class="myButton">REGISTER</a>
+<br><br>
+<span style="color: #fff; font-size: 28px;"><b>SELAMAT DATANG DI PARLAY4D</b></span><br><br>
+<span style="color:#ffee00; font-size:30px;">TOTAL JACKPOT</span> <span id="jp" style=" color:#fff; font-size:32px;">1,284,578,503</span>
+<br><br>
+
+<a href="/" title="BACCARAT ONLINE" ><img src="https://i.postimg.cc/bNMxQBKX/image-2021-10-31-222637.png" alt="Baccarat Online" title="Baccarat Online" width="195"></a>
+<a href="/" title="Sicbo Online" ><img src="https://i.postimg.cc/G3jrT0T2/image-2021-10-31-222948.png" alt="Sicbo Online" title="Sicbo Online" width="190"></a>
+<a href="https://i.postimg.cc/G3jrT0T2/image-2021-10-31-222948.png" title="Roulette Online" ><img src="https://i.postimg.cc/23p4fqdk/image-2021-10-31-223411.png" alt="Roulette Online" title="Roulette Online" width="145"></a>
+<a href="/" title="Dragon Tiger Online" ><img src="https://i.postimg.cc/sXjzwKty/image-2021-10-31-223748.png" alt="Dragon Tiger Online" title="Dragon Tiger Online" width="190"></a><br>
+<a href="/" title="Slot Games" ><img src="https://i.postimg.cc/Gt3k30VN/image-2021-10-31-224543.png" alt="Slot Games" title="Slot Games" width="205"></a>
+<a href="/" title="Afb Sport" ><img src="https://i.postimg.cc/gkCnNF4W/image-2021-10-31-224821.png" alt="Afb Sport" title="Afb Sport" width="255"></a>
+<a href="/" title="W1poker" ><img src="https://i.postimg.cc/C5Zxsmq5/image-2021-10-31-225017.png" alt="W1poker" title="W1poker" width="155"></a>
+<a href="/" title="Afb Gaming" ><img src="https://i.postimg.cc/L4BprdHS/image-2021-10-31-225242.png" alt="Afb Gaming" title="Afb Gaming" width="155"></a>
+<div id="content" class="hfeed" style="font-family:Arial;font-size:16px;max-width:750px;padding:0px 50px;margin:0;">
+
+<h1 itemprop="headline" class="entry-title" style="color: #ffffff;">Situs PARLAY4D BOLA Online Terbesar di Indonesia, W1POKER Online, SLOT , AFB, AFB GAMING, Togel Online Di Indonesia</h1>
+<p itemprop="text" class="entry-content" style="color: #fff; font-size:15px;">PARLAY4D merupakan Situs judi <a href="http://parlay4d.cc/?ref=yoyo1123" style="color:#ffee00;text-decoration:none;font-weight:bold;"/>BOLA Online</a> terbaik dan terpercaya di Indonesia! PARLAY4D didukung dengan sistem server terbaik yang mampu menjamin keamanan data semua membernya MPO. Silakan Daftar menggunakan Link PARLAY4D yang terpercaya demi menghindari hal yang tidak di inginkan terjadi!! Gabung yuk di Login PARLAY4D</p>
+<h2 style="color: #ffffff;">Daftar PARLAY4D, Link Alternatif Resmi PARLAY4D, TOGEL ONLINE, Bola Online Terbesar Di Indonesia</h2>
+<p style="color: #fff; font-size:15px;">PARLAU4D mengutamakan kenyamanan para membernya saat melakukan deposit maupun withdraw dengan pelayanan aman dengan minimal deposit hanya 15.000 IDR . Tidak hanya permainan <a href="http://parlay4d.cc/?ref=yoyo1123"  style="color:#ffee00;text-decoration:none;"/>SLOT Online</a> yang sangat diminati dan sangat mudah dimenangkan. Situs PARLAY4D memberi berbagai bonus yang sangat menarik yang bisa kamu temukan hanya disini PARLAY4D..!</p>
+<h2 style="color: #ffffff;">Sistem Permainan PARLAY4D Murni 100% SLOT , live casino , live bola tepercaya togel online !</h2>
+<p style="color: #fff; font-size:15px;">Situs yang telah kami rekomendasikan merupakan situs yang telah terjamin tingkat keamanan dengan menggunakan teknologi terbaru dibidangnya, Tunjukan kemampuan kamu dalam bermain dengan bergabung dan Daftar bersama Situs yang kami rekomendasikan disini PARLAY4D!!!</p><br><br>
+
+<div id="footer" itemscope="itemscope" itemtype="https://schema.org/WPFooter">
+            <div class="wraper">
+                <div style="float: center">
+                </div>
+                <div style="float: center">
+                    <ul id="menu-footer">
+                        <li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">Baccarat Online</a></li>
+                        <li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">Sicbo Online </a></li>
+                        <li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">Roulette Online</a></li>
+                        <li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">Dragon Tiger</a></li>
+                        <li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">Slot Games</a></li>
+						<li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">Afb Sport</a></li>
+						<li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">W1poker</a></li>
+						<li><a itemprop="url" title="PARLAY4D" href="" style="text-decoration:none;">Afb Gaming</a></li>
+                        
+
+                    </ul>
+                    <div style="color: #fff; text-align: center;">© 2019 PARLAY4D. All Rights Reserved.</div>
+                </div>
+            </div>
+        </div></span></div></center></td>
+   </tr>
+</tbody></table>
+</center>
+
+</script></body>
+</html>
